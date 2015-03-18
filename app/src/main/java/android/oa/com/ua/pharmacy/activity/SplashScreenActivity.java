4950b5706e1b2fb275658b59b7d21ea6e248ee7d
igor9052 <igor905.2@gmail.com>
2015-03-18 09:48:37 +0200

    Downloading json data file from internet was implemented.

--- app/src/main/java/android/oa/com/ua/pharmacy/activity/SplashScreenActivity.java
+++ app/src/main/java/android/oa/com/ua/pharmacy/activity/SplashScreenActivity.java
@@ -1,41 +1,50 @@
 package android.oa.com.ua.pharmacy.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.oa.com.ua.pharmacy.R;
+import android.oa.com.ua.pharmacy.util.Settings;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ProgressBar;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class SplashScreenActivity extends Activity {
+
+    private static GetPackageTask getPackageTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //Emulation resource utilization
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                try {
-                    ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-                    for (int i = 0; i < 100; i += 10) {
-                        progressBar.setProgress(i);
-                        Thread.sleep(200);
-                    }
-                    Intent intent = new Intent(SplashScreenActivity.this, CatalogActivity.class);
-                    startActivity(intent);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    // close this activity
-                    finish();
-                }
-            }
-        }).start();
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if (checkInitPackage()) {
+            progressBar.setProgress(100);
+            Intent intent = new Intent(SplashScreenActivity.this, CatalogActivity.class);
+            startActivity(intent);
+            return;
+        }
+
+        /*If GetPackage AsyncTask is active then getPackageTask != null, thus we just add progressBar
+         * and activity to it. Otherwise the new AsyncTask is started. */
+        if (getPackageTask == null) {
+            getPackageTask = new GetPackageTask(this, progressBar);
+            getPackageTask.execute(getApplication());
+        } else {
+            getPackageTask.setProgressBar(progressBar);
+            getPackageTask.setActivity(this);
+        }
     }
 
     @Override
@@ -43,4 +52,97 @@ public class SplashScreenActivity extends Activity {
         super.onPause();
     }
 
+    /*Check whether data file had been downloaded and stored in the Internal Memory*/
+    private boolean checkInitPackage() {
+        for (String item : fileList()) {
+            if (Settings.DATA_FILE.equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*This AsyncTask downloads json file from internet and stores it to Internal Memory*/
+    private static class GetPackageTask extends AsyncTask<Context, Integer, Void> {
+
+        ProgressBar progressBar;
+        Activity activity;
+
+        private GetPackageTask(Activity activity, ProgressBar progressBar) {
+            this.progressBar = progressBar;
+            this.activity = activity;
+        }
+
+        public void setProgressBar(ProgressBar progressBar) {
+            this.progressBar = progressBar;
+        }
+
+        public void setActivity(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setProgress(0);
+        }
+
+        @Override
+        protected Void doInBackground(Context... params) {
+            int totalSize;
+            int downloadedSize;
+            byte[] buffer;
+            int bufferLength;
+            URL url;
+            HttpURLConnection urlConnection;
+            InputStream inputStream;
+
+            FileOutputStream fos;
+
+            try {
+                url = new URL(Settings.Api.BASE_URL + Settings.DATA_FILE);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+
+                fos = params[0].openFileOutput(Settings.DATA_FILE, MODE_PRIVATE);
+                inputStream = urlConnection.getInputStream();
+
+                totalSize = urlConnection.getContentLength();
+                downloadedSize = 0;
+                buffer = new byte[totalSize / 10];
+
+                boolean isWiFi = ((ConnectivityManager) params[0].getSystemService(Context.CONNECTIVITY_SERVICE))
+                        .getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI;
+
+                while ((bufferLength = inputStream.read(buffer)) > 0) {
+                    fos.write(buffer, 0, bufferLength);
+                    downloadedSize += bufferLength;
+                    publishProgress(downloadedSize, totalSize);
+                    if (isWiFi) {
+                        Thread.sleep(500);
+                    }
+                }
+                fos.close();
+                inputStream.close();
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress((int) ((values[0] / (float) values[1]) * 100));
+            Log.i("Downloading", String.valueOf(((int) ((values[0] / (float) values[1]) * 100))));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            getPackageTask = null;
+            Intent intent = new Intent(activity, CatalogActivity.class);
+            activity.startActivity(intent);
+        }
+    }
 }
---
+++ app/src/main/java/android/oa/com/ua/pharmacy/util/Settings.java
@@ -0,0 +1,14 @@
+package android.oa.com.ua.pharmacy.util;
+
+/**
+ * Created by Igor Kuzmenko on 10.03.2015.
+ */
+public class Settings {
+
+    public static final String DATA_FILE = "pharmacy.json";
+
+    public static class Api {
+        public static final String BASE_URL = "https://s3.eu-central-1.amazonaws.com/igorka/pharmacy/";
+    }
+
+}
